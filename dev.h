086caac37b9d11bea9571b4f5e12c0d4053308bd
accessrr <accessramesh@gmail.com>
2017-09-13 17:09:13 +0530

    [Ramkumar]Changing Driver version from P16 => P17 for release

--- dev.h
+++ dev.h
@@ -30,7 +30,7 @@
 #include "pfu.h"
 
 #define MWL_DRV_NAME     KBUILD_MODNAME
-#define MWL_DRV_VERSION	 "P16-20170908"
+#define MWL_DRV_VERSION	 "P17-20170913"
 
 
 /* Map to 0x80000000 (Bus control) on BAR0 */
