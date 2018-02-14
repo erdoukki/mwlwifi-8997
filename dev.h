2a0e2704c96342a0bb7f8a8105d749369d0326c7
Sharvari Harisangam <Sharvari@marvell.com>
2018-02-14 18:53:38 +0530

    Changing Driver version from P20 => P21 for release

--- dev.h
+++ dev.h
@@ -30,7 +30,7 @@
 #include "pfu.h"
 
 #define MWL_DRV_NAME     KBUILD_MODNAME
-#define MWL_DRV_VERSION	 "P20-20180105"
+#define MWL_DRV_VERSION	 "P21-20180214"
 
 
 /* Map to 0x80000000 (Bus control) on BAR0 */
