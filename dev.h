d48046b220142c53498bc147143e123f3511882b
Ramesh Radhakrishnan <rramesh@marvell.com>
2018-03-19 18:37:50 +0530

    Changing Driver version from P22 => P23 for release

--- dev.h
+++ dev.h
@@ -30,7 +30,7 @@
 #include "pfu.h"
 
 #define MWL_DRV_NAME     KBUILD_MODNAME
-#define MWL_DRV_VERSION	 "P22-20180219"
+#define MWL_DRV_VERSION	 "P23-20180319"
 
 
 /* Map to 0x80000000 (Bus control) on BAR0 */
