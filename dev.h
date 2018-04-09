5865ab23ad8b7187418a30442a4deab1a361748a
accessrr <accessramesh@gmail.com>
2018-04-09 12:15:30 +0000

    [Ramkumar]Changing driver version from P26=>P27

--- dev.h
+++ dev.h
@@ -30,7 +30,7 @@
 #include "pfu.h"
 
 #define MWL_DRV_NAME     KBUILD_MODNAME
-#define MWL_DRV_VERSION	 "P26-20180403"
+#define MWL_DRV_VERSION	 "P27-20180409"
 
 
 /* Map to 0x80000000 (Bus control) on BAR0 */
