4c8b07222dfb0960e18f958a945c0dd7d2396d52
accessrr <rramesh@marvell.com>
2018-08-09 15:08:54 +0530

    [Ramkumar]Changing Drv version from P32=>P33 for build release

--- dev.h
+++ dev.h
@@ -30,7 +30,7 @@
 #include "pfu.h"
 
 #define MWL_DRV_NAME     KBUILD_MODNAME
-#define MWL_DRV_VERSION	 "P32-20180802"
+#define MWL_DRV_VERSION	 "P33-20180809"
 
 
 /* Map to 0x80000000 (Bus control) on BAR0 */
